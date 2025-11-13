8bf2b530f655a427ab28172ac0b7a35d7355102c
Saif-Smran <23303079@iubat.edu>
2025-11-13 20:28:34 +0600

    Tamplate complited

--- caiheadq/caiheadq/settings.py
+++ caiheadq/caiheadq/settings.py
@@ -127,4 +127,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATUC_URL = '/static/'
-STATICFILS_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
